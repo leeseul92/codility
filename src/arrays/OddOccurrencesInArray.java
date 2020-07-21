82fcd20a30e36c0ecd5558e1825f3fe67db4a0be
Leeseul Kim <chloe92222@gmail.com>
2020-07-21 21:06:36 +0900

    [배열 두번째문제 수정]

--- src/arrays/OddOccurrencesInArray.java
+++ src/arrays/OddOccurrencesInArray.java
@@ -1,8 +1,27 @@
 package arrays;
+
+import java.util.*;
+
 class OddOccurrencesInArray {
     public int solution(int[] A){
         //stack 사용
-        
-        return 0;
+    	Stack<Integer> stack = new Stack<Integer>();
+    	
+//    	//첫번째 배열의 값을 스택에 push해준다.
+//    	stack.push(A[0]);
+    	
+    	for(int i = 0; i < A.length; i++) {
+    		if(stack.isEmpty()) {
+    			stack.push(A[i]);
+    		} else {
+    			if(stack.peek() == A[i]) {
+    				stack.pop();
+    			} else {
+    				stack.push(A[i]);
+    			}
+    		}
+    	}
+    	
+        return stack.peek();
     }
 }
