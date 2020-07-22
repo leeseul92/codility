60ae7fa2c8225734bd1085bc41bc6f6573b59b9f
unknown <chloe92222@gmail.com>
2020-07-22 14:31:32 +0900

    [OddArray 3rd]

--- src/arrays/OddOccurrencesInArray02.java
+++ src/arrays/OddOccurrencesInArray02.java
@@ -2,22 +2,27 @@ package arrays;
 
 import java.util.*;
 
+import javax.swing.*;
+
 public class OddOccurrencesInArray02 {
 	public int solution(int[] A) {
 		//List 사용
-		List<Integer> list = new ArrayList<Integer>();
-		
-		for(int i = 0; i < A.length; i++) {
-			if(list.isEmpty()) {
-				list.add(A[i]);
-			} else {
-				for(int j = 0; j < list.size(); j++) {
-					if(list.get(j) == A[i]) {
-						
-					}
+		// List<Integer> list = new ArrayList<Integer>();
+		//HashSet 사용
+		HashSet<Integer> hSet = new HashSet<Integer>();
+
+		for(int i = 0; i < A.length; i++){
+			if(hSet.isEmpty()){
+				hSet.add(A[i]);
+			}else{
+				if(hSet.add(A[i]) == false){
+					hSet.remove(A[i]);
 				}
 			}
 		}
-		return 0;
+
+		Iterator<Integer> itor = hSet.iterator();
+		
+		return itor.next();
 	}
 }
